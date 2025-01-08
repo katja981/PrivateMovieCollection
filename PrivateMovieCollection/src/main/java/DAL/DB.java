aad9d9bdb0df9f01964d2d80c334f8404ee16034
katja981 <katstr01@easv365.dk>
2025-01-08 12:19:29 +0100

    I have added a connection to the database under the class DB. I've also made small changes to module-info.java and the Pom-file to make it work.

--- PrivateMovieCollection/src/main/java/DAL/DB.java
+++ PrivateMovieCollection/src/main/java/DAL/DB.java
@@ -1,4 +1,21 @@
 package DAL;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
+
 public class DB {
+    public Connection DBConnection() throws SQLServerException {
+        SQLServerDataSource ds;
+        ds = new SQLServerDataSource();
+        ds.setDatabaseName("PrivateMovieCollection_Group5_int");
+        ds.setUser("CSe2024b_e_15");
+        ds.setPassword("CSe2024bE15!24");
+        ds.setServerName("EASV-DB4");
+        ds.setPortNumber(1433);
+        ds.setTrustServerCertificate(true);
+        return ds.getConnection();
+    }
 }
--- PrivateMovieCollection/src/main/java/module-info.java
+++ PrivateMovieCollection/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module dk.easv.privatemoviecollection {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
+    requires java.naming;
+
 
 
     opens dk.easv.privatemoviecollection to javafx.fxml;
