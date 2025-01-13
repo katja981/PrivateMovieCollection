e8003e40bc343a64fa0f28c5140982f046f26563
lauraaa05 <laushp01@easv365.dk>
2025-01-13 11:06:52 +0100

    Fixed issue with the App not being the right size.

--- PrivateMovieCollection/src/main/java/dk/easv/privatemoviecollection/HelloApplication.java
+++ PrivateMovieCollection/src/main/java/dk/easv/privatemoviecollection/HelloApplication.java
@@ -13,8 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        StackPane root = fxmlLoader.load();
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello World");
         stage.setScene(scene);
         stage.show();
