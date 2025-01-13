ecbd00a14d5c229fdb97a3899e0424d5926e11a2
lauraaa05 <laushp01@easv365.dk>
2025-01-13 10:59:35 +0100

    New FXML with popup windows (NEEDS CODE FOR WINDOWS TO POPUP)

--- PrivateMovieCollection/src/main/java/dk/easv/privatemoviecollection/HelloApplication.java
+++ PrivateMovieCollection/src/main/java/dk/easv/privatemoviecollection/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        BorderPane root = fxmlLoader.load();
+        StackPane root = fxmlLoader.load();
         Scene scene = new Scene(root, 600, 400);
         stage.setTitle("Hello World");
         stage.setScene(scene);
