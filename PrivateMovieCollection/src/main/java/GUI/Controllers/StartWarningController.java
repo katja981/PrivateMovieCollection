cfadbc0d7037e1b4fb5bf293795f2ed839d20ee2
ermartin23 <148077979+ermartin23@users.noreply.github.com>
2025-01-16 15:09:49 +0100

    made warning pop

---
+++ PrivateMovieCollection/src/main/java/GUI/Controllers/StartWarningController.java
@@ -0,0 +1,11 @@
+package GUI.Controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartWarningController {
+}
